616bd2e45933564c3958b34d90616cf1e8a91268
Danielbril2 <brildan@post.bgu.ac.il>
2025-02-26 00:42:08 +0200

    commented previous models

--- src/main/py/Proj/app/models/shoe.py
+++ src/main/py/Proj/app/models/shoe.py
@@ -1,23 +1,23 @@
-import enum
-from app import db
+# import enum
+# from app import db
 
-class ShoeType(enum.Enum):
-    Man = "Man"
-    Woman = "Woman"
-    Children = "Children"
+# class ShoeType(enum.Enum):
+#     Man = "Man"
+#     Woman = "Woman"
+#     Children = "Children"
 
-class Shoe(db.Model):
-    code = db.Column(db.String(50), primary_key=True)
-    loc = db.Column(db.Integer, nullable=True)
-    name = db.Column(db.String(100), nullable=True)
-    type = db.Column(db.Enum(ShoeType), nullable=True)
-    image = db.Column(db.LargeBinary, nullable=True)
+# class Shoe(db.Model):
+#     code = db.Column(db.String(50), primary_key=True)
+#     loc = db.Column(db.Integer, nullable=True)
+#     name = db.Column(db.String(100), nullable=True)
+#     type = db.Column(db.Enum(ShoeType), nullable=True)
+#     image = db.Column(db.LargeBinary, nullable=True)
     
-    def to_dict(self):
-        return {
-            'code': self.code,
-            'loc': self.loc,
-            'name': self.name,
-            'type': self.type.name if self.type else None,
-            'image': self.image if self.image else None
-        }
+#     def to_dict(self):
+#         return {
+#             'code': self.code,
+#             'loc': self.loc,
+#             'name': self.name,
+#             'type': self.type.name if self.type else None,
+#             'image': self.image if self.image else None
+#         }
--- src/main/py/Proj/app/models/user.py
+++ src/main/py/Proj/app/models/user.py
@@ -1,22 +1,22 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-from app import db
+# from werkzeug.security import generate_password_hash, check_password_hash
+# from app import db
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    worker_code = db.Column(db.String(50), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     worker_code = db.Column(db.String(50), unique=True, nullable=False)
+#     password_hash = db.Column(db.String(128))
     
-    def __init__(self, worker_code, password):
-        self.worker_code = worker_code
-        self.set_password(password)
+#     def __init__(self, worker_code, password):
+#         self.worker_code = worker_code
+#         self.set_password(password)
     
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+#     def set_password(self, password):
+#         self.password_hash = generate_password_hash(password)
     
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+#     def check_password(self, password):
+#         return check_password_hash(self.password_hash, password)
     
-    @staticmethod
-    def confirm_password(password):
-        # can also check if password == 52500219 but its boring
-        return check_password_hash(generate_password_hash("52500219"), password)
+#     @staticmethod
+#     def confirm_password(password):
+#         # can also check if password == 52500219 but its boring
+#         return check_password_hash(generate_password_hash("52500219"), password)
