c33449f0005c58e9bfd8f2d21c244c670472e210
Danielbril2 <brildan@post.bgu.ac.il>
2025-02-27 18:54:28 +0200

    Some db related changes

--- src/main/py/Proj/app/__init__.py
+++ src/main/py/Proj/app/__init__.py
@@ -18,6 +18,15 @@ def create_app(config_class=Config):
     CORS(app)
     #db.init_app(app)
     mongo.init_app(app)
+
+    with app.app_context():
+        try:
+            # Test MongoDB connection
+            mongo.db.command('ping')
+            print("MongoDB connection successful!")
+            print(f"Available collections: {mongo.db.list_collection_names()}")
+        except Exception as e:
+            print(f"MongoDB connection failed: {str(e)}")
     
     # Register blueprints
     from app.controllers.auth_controller import auth_bp
@@ -25,9 +34,5 @@ def create_app(config_class=Config):
     
     app.register_blueprint(auth_bp, url_prefix='/auth')
     app.register_blueprint(shoe_bp, url_prefix='/main')
-    
-    # Create database tables
-    #with app.app_context():
-        #db.create_all()
         
     return app
--- src/main/py/Proj/app/config.py
+++ src/main/py/Proj/app/config.py
@@ -1,7 +1,9 @@
 class Config:
     SECRET_KEY = 'YourVeryLongAndSecureSecretKeyThatShouldBeAtLeast256BitsLong'
 
-    MONGO_URI = 'mongodb+srv://danielbril054:BKNz0rb2GIWMwc7w@shoesdb.wu9xu.mongodb.net/?retryWrites=true&w=majority&appName=ShoesDB'
+    #MONGO_URI = 'mongodb+srv://danielbril054:BKNz0rb2GIWMwc7w@shoesdb.wu9xu.mongodb.net/?retryWrites=true&w=majority&appName=ShoesDB'
+    MONGO_URI = 'mongodb+srv://danielbril054:BKNz0rb2GIWMwc7w@shoesdb.wu9xu.mongodb.net/nike_shoes?retryWrites=true&w=majority'
+
     #do not need what goes here - but keeping it just in case...
     SQLALCHEMY_DATABASE_URI = 'sqlite:///nike_shoes.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
